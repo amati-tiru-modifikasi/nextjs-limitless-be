3c9caafbaef03e45ce75347b4ce7e7920ed5eada
mdestafadilah <desta.08b@gmail.com>
2022-04-14 13:57:55 +0700

    LINKEDIN https://www.linkedin.com/company/rsup-persahabatan

--- blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
+++ blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { NextApiHandler } from "next";
 import NextAuth from "next-auth/next";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
+import LinkedInProvider from "next-auth/providers/linkedin";
 import prisma from "../../../lib/prisma";
 
 const authHanlder: NextApiHandler = (req, res) => NextAuth(req, res, options);
@@ -12,6 +13,10 @@ const options = {
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+        }),
+        LinkedInProvider({
+            clientId: process.env.LINKEDIN_CLIENT_ID,
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET
         })
     ],
     adapter: PrismaAdapter(prisma),
